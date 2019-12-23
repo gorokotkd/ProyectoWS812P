af079914283e191dc3af092d1f9e107289e8076e
Gorka Alvarez <galvarez024@ikasle.ehu.eus>
2019-12-23 12:05:02 +0100

    Corregido Error en ClientVerifyPass

--- php/SignUp.php
+++ php/SignUp.php
@@ -84,6 +84,7 @@
                 die("Fallo al conectar a MySQL: " .mysqli_connect_error());
             }
             
+            
             $tipo = $_REQUEST['tipoUsu'];
             $email = $_REQUEST['dirCorreo'];
             $nombreApellidos = $_REQUEST['nombreApellidos'];
@@ -94,7 +95,14 @@
             }else{
                 $image = $_FILES['Imagen']['tmp_name'];             
             }
-            
+            //Comprobamos si existe un usuario con ese email.
+            $sql = "SELECT FROM usuarios where email='{$email}';";
+            $resul = mysqli_query($mysqli,$sql,MYSQLI_USE_RESULT);
+            if(!$resul)
+            {
+                die("<h4 style=\"color: red\">Ese email ya esta en uso.</h4><p>Si no recuerdas la contraseña de tu email pulsa en este <a href=\"#\">enlace</a> para restablecerla.</p>");
+            }
+
             $contenido_imagen = base64_encode(file_get_contents($image));
             $sql = "INSERT INTO usuarios VALUES ($tipo,'$email','$nombreApellidos','$hashed_password','$contenido_imagen','1');";
             
